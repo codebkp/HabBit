dbaee7ea54c3123e932349918b68fe5cd5949f94
ArachisH <TheOldNutMan@gmail.com>
2016-08-15 22:50:43 -0700

    Code cleanup.

--- HabBit/Program.cs
+++ HabBit/Program.cs
@@ -5,21 +5,20 @@ using System.Reflection;
 using System.Diagnostics;
 using System.Collections.Generic;
 
-using CommandLine;
-using CommandLine.Text;
+using HabBit.Habbo;
+using HabBit.Utilities;
 
 using FlashInspect;
 using FlashInspect.ActionScript;
 
-using HabBit.Habbo;
-using HabBit.Utilities;
+using CommandLine;
+using CommandLine.Text;
 
 namespace HabBit
 {
     public class Program
     {
         private readonly HBRSAKeys _keys;
-        private readonly Stopwatch _watch;
 
         private const string DEFAULT_EXPONENT = "3";
         private const string DEFAULT_MODULUS = "86851dd364d5c5cece3c883171cc6ddc5760779b992482bd1e20dd296888df91b33b936a7b93f06d29e8870f703a216257dec7c81de0058fea4cc5116f75e6efc4e9113513e45357dc3fd43d4efab5963ef178b78bd61e81a14c603b24c8bcce0a12230b320045498edc29282ff0603bc7b7dae8fc1b05b52b2f301a9dc783b7";
@@ -32,8 +31,6 @@ namespace HabBit
 
         public Program(HBOptions options)
         {
-            _watch = new Stopwatch();
-
             Options = options;
             Game = new HGame(Options.GamePath);
 
@@ -52,7 +49,7 @@ namespace HabBit
 
             if (Options.RSAKeySize != null)
             {
-                Console.Write($"Generating {Options.RSAKeySize}-bit RSA Keys...");
+                Console.WriteLine($"Generating {Options.RSAKeySize}-bit RSA Keys...");
                 _keys = new HBRSAKeys((int)Options.RSAKeySize);
                 WriteLineSplit();
             }
@@ -78,18 +75,17 @@ namespace HabBit
             {
                 var options = new HBOptions();
                 bool parsed = Parser.Default.ParseArguments(args, options);
-                if (parsed)
+
+                if (!parsed)
                 {
-                    new Program(options).Run();
-                }
-                else
-                {
+
                     var help = HelpText.AutoBuild(options);
                     help.Heading = $"HabBit[Version {GetVersion()}]";
                     help.Copyright = $"Copyright (c) 2016 ArachisH";
                     help.MaximumDisplayWidth = Console.WindowWidth;
                     Console.WriteLine(help);
                 }
+                else new Program(options).Run();
             }
             finally { Console.CursorVisible = true; }
         }
@@ -110,8 +106,10 @@ namespace HabBit
             /* Step #3 - Compression/Assembling */
             Assemble();
 
-            Console.Write("Cleaning Up...");
+            Console.WriteLine("Cleaning Up...");
+            Console.WriteLine();
 
+            Console.WriteLine("    Saving RSA Keys...");
             using (var rsaKeyWriter = new StreamWriter(
                 Path.Combine(Options.OutputPath, "RSAKeys.txt")))
             {
@@ -122,6 +120,7 @@ namespace HabBit
 
             if (Options.IsDumpingHeaders)
             {
+                Console.WriteLine("    Dumping Headers...");
                 using (var headersWriter = new StreamWriter(
                     Path.Combine(Options.OutputPath, "Headers.txt")))
                 {
@@ -138,19 +137,19 @@ namespace HabBit
             WriteLineSplit();
 
             globalWatch.Stop();
-            Console.WriteLine("Completion Time: " + GetLap(globalWatch));
+            Console.WriteLine("Completion Time: {0:s\\.ff} Seconds", globalWatch.Elapsed);
         }
         public void Modify()
         {
-            _watch.Restart();
             Console.WriteLine("Modifying...");
+            Console.WriteLine();
 
             if (!string.IsNullOrWhiteSpace(Options.Revision))
             {
-                string oldRevision = Game.Revision;
                 Game.Revision = Options.Revision;
 
-                Console.WriteLine($"     Revision Changed: {oldRevision} -> {Game.Revision}");
+                Console.WriteLine(
+                    $"    Revision Changed: \"{Game.Revision}\"");
             }
 
             bool replacedPatterns = false;
@@ -167,28 +166,26 @@ namespace HabBit
                     }
 
                     replacedPatterns = true;
-                    string oldPattern = Game.ValidHostsRegexPatterns[i];
                     Game.ValidHostsRegexPatterns[i] = newPattern;
 
-                    Console.WriteLine($"     Regex Pattern Changed: {oldPattern} -> {newPattern}");
+                    Console.WriteLine(
+                        $"    Valid Host Pattern Changed[{i}]: \"{newPattern}\"");
                 }
             }
 
-            Console.Write("     Bypassing Domain Checks...");
-            Game.BypassDomainChecks(replacedPatterns).WriteLineResult();
-
-            Console.Write("     Replacing RSA Keys...");
+            Console.Write("    Replacing RSA Keys...");
             Game.ReplaceRSAKeys(_keys.Exponent, _keys.Modulus).WriteLineResult();
 
-            WriteLineSplit(GetLap());
+            Console.Write("    Bypassing Domain Checks...");
+            Game.BypassDomainChecks(replacedPatterns).WriteLineResult();
+
+            WriteLineSplit();
         }
         public void Assemble()
         {
-            _watch.Restart();
-            Console.Write("Assembling...");
-
+            Console.WriteLine("Assembling...");
             Game.Assemble();
-            WriteLineSplit(" | " + GetLap());
+            WriteLineSplit();
 
             byte[] compressed = null;
             if (Options.Compression != null &&
@@ -197,11 +194,9 @@ namespace HabBit
                 Game.Compression =
                     (CompressionType)Options.Compression;
 
-                _watch.Restart();
-                Console.Write("Compressing({0})...", Game.Compression);
-
+                Console.WriteLine("Compressing({0})...", Game.Compression);
                 compressed = Game.Compress();
-                WriteLineSplit(" | " + GetLap());
+                WriteLineSplit();
             }
 
             string assembledPath = Path.Combine(
@@ -214,39 +209,33 @@ namespace HabBit
         {
             if (Game.IsCompressed)
             {
-                _watch.Restart();
-                Console.Write("Decompressing({0})...", Game.Compression);
-
+                Console.WriteLine("Decompressing({0})...", Game.Compression);
                 Game.Decompress();
-                WriteLineSplit(" | " + GetLap());
+                WriteLineSplit();
             }
         }
         public void Disassemble()
         {
-            _watch.Restart();
-            Console.Write("Disassembling...");
-
+            Console.WriteLine("Disassembling...");
             Game.Disassemble();
-            WriteLineSplit(" | " + GetLap());
+            WriteLineSplit();
         }
 
-        private string GetLap()
+        private void WriteSplit()
         {
-            return GetLap(_watch);
+            Console.Write("---------------");
         }
-        private string GetLap(Stopwatch watch)
+        private void WriteLineSplit()
         {
-            watch.Stop();
-            return $"{watch.Elapsed:s\\.ff} Seconds";
+            WriteSplit();
+            Console.WriteLine();
         }
-
-        private void WriteMessages(string title,
-            IReadOnlyDictionary<ushort, ASClass> messages,
-            StreamWriter writer)
+        private void WriteMessages(string title, IReadOnlyDictionary<ushort, ASClass> messages, StreamWriter writer)
         {
             foreach (ushort header in messages.Keys)
             {
-                ASInstance msgInstance = messages[header].Instance;
+                ASClass msgClass = messages[header];
+                ASInstance msgInstance = msgClass.Instance;
                 string msgName = msgInstance.QName.Name;
 
                 writer.WriteLine("{0}[{1}] = {2}",
@@ -269,16 +258,6 @@ namespace HabBit
             titleBuilder.Append(Path.GetFileName(flash.Location));
             titleBuilder.AppendFormat("({0:00.0} MB)", BytesToMB(flash.FileLength));
             Console.Title += titleBuilder;
-        }
-
-        private void WriteLineSplit()
-        {
-            WriteLineSplit(string.Empty);
-        }
-        private void WriteLineSplit(string value)
-        {
-            Console.WriteLine(value);
-            Console.WriteLine("---------------");
         }
     }
 }
